b1f572c67b5bd46c588487fa947d76951532f21e
Imam Susanto <abc.imams@gmail.com>
2018-03-22 20:41:49 +0700

    Update View

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<div class="">
 	  <div class="container" style="max-width:800px; margin-top:25vh">
-	    <h1>Selamat datang di aplikasi Pasar Modal <span class="label label-success">v2.1</span></h1>
+	    <h1>Selamat datang di aplikasi Pasar Modal <span class="badge badge-success align-text-bottom">v2.2</span></h1>
 	    <p class="lead">Aplikasi ini bertujuan untuk memudahkan pengguna untuk mengukur sejauh mana kinerja Usaha Mikro Kecil Menengah agar mereka mampu memasuki Pasar Modal</p>
 	    <p>
 	    @auth
--- resources/views/home/panduan.blade.php
+++ resources/views/home/panduan.blade.php
@@ -10,7 +10,7 @@
       <h3 class="pt-4" id="login">Login/Logout</h3>
       <p>Untuk dapat menggunakan aplikasi ini, Anda diharuskan untuk login. Untuk melakukan login, silakan klik tombol login (Gambar 1) untuk masuk halaman login.</p>
       <a href="/img/Image-001.png">
-        <img src="/img/Image-001.png" class="img-responsive" alt="Gambar 1">
+        <img src="/img/Image-001.png" class="img-fluid" alt="Gambar 1">
       </a>
       <p style="text-align: center"><small>Gambar 1</small></p>
       <p>Pada halaman login (Gambar 2) masukkan kredensial sebagai berikut:</p>
@@ -20,32 +20,32 @@
           <code>password: umkm</code></pre>
       </p>
       <a href="/img/Image-002.png">
-        <img src="/img/Image-002.png" class="img-responsive" alt="Gambar 2">
+        <img src="/img/Image-002.png" class="img-fluid" alt="Gambar 2">
       </a>
       <p style="text-align: center"><small>Gambar 2</small></p>
       <p>Jika sudah login, maka Anda akan diarahkan ke halaman dashboard kuesioner.</p>
       <h3 class="pt-4" id="dasbor">Halaman Dasbor</h3>
       <p>Pada halaman dasbor (Gambar 3), Anda akan menemukan kumpulan data kuesioner yang telah Anda masukkan sebelumnya. Di halaman ini Anda dapat menambah kuesioner baru, mengedit kuesioner, melihat detil kuesioner, dan menghapus kuesioner.</p>
       <a href="/img/Image-005.png">
-        <img src="/img/Image-005.png" class="img-responsive" alt="Gambar 3">
+        <img src="/img/Image-005.png" class="img-fluid" alt="Gambar 3">
       </a>
       <p style="text-align: center"><small>Gambar 3</small></p>
       <h3 class="pt-4" id="buat">Membuat Kuesioner</h3>
       <p>Pada halaman pembuatan kuesioner (Gambar 4), Anda akan diminta untuk menginput data yang dibutuhkan oleh kuesioner. Tidak semua data perlu dimasukkan. Pada saat melakukan submit, aplikasi akan memberikan peringatan validasi jika ada data yang diperlukan belum dimasukkan.</p>
       <a href="/img/Image-004.png">
-        <img src="/img/Image-004.png" class="img-responsive" alt="Gambar 4">
+        <img src="/img/Image-004.png" class="img-fluid" alt="Gambar 4">
       </a>
       <p style="text-align: center"><small>Gambar 4</small></p>
       <h3 class="pt-4" id="detil">Detil Kuesioner</h3>
       <p>Halaman ini (Gambar 5) menjelaskan informasi merupakan hasil kalkulasi dan analisis dari data kuesioner yang telah dimasukkan sebelumnya. Laporan berupa Data Umum, Aspek Keuangan, Aspek Manajerial, dan Hasil.</p>
       <a href="/img/Image-006.png">
-        <img src="/img/Image-006.png" class="img-responsive" alt="Gambar 5">
+        <img src="/img/Image-006.png" class="img-fluid" alt="Gambar 5">
       </a>
       <p style="text-align: center"><small>Gambar 5</small></p>
       <h3 class="pt-4" id="edit">Mengedit Kuesioner</h3>
       <p>Pada halaman pengeditan kuesioner (Gambar 6), Anda dapat memanipulasi data yang sudah Anda masukkan. Gunakan tombol edit di bagian bawah form untuk mengkonfirmasi perubahan yang sudah dilakukan.</p>
       <a href="/img/Image-007.png">
-        <img src="/img/Image-007.png" class="img-responsive" alt="Gambar 6">
+        <img src="/img/Image-007.png" class="img-fluid" alt="Gambar 6">
       </a>
       <p style="text-align: center"><small>Gambar 6</small></p>
       <h3 class="pt-4" id="hapus">Menghapus Kuesioner</h3>
@@ -53,7 +53,7 @@
       <h3 class="pt-4" id="print">Mengeprint Kuesioner</h3>
       <p>Kuesioner yang telah dibuat dapat disimpan sebagai dokumen atau dicetak (Gambar 7). Caranya adalah menekan tombol print pada halaman dasbor kuesioner dan halaman detil kuesioner. Hasilnya berupa dokumen pdf yang dapat disimpan dan dapat dicetak langsung.</p>
       <a href="/img/Image-008.png">
-        <img src="/img/Image-008.png" class="img-responsive" alt="Gambar 7">
+        <img src="/img/Image-008.png" class="img-fluid" alt="Gambar 7">
       </a>
       <p style="text-align: center"><small>Gambar 7</small></p>
     </div>
